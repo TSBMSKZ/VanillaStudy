5124cde861b1c6379cd70024d7f2f479a8aabfa3
TSB <147370057+TSBMSKZ@users.noreply.github.com>
2023-10-17 11:53:57 +0000

    Update 6 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,14 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  //テキストの値を変数に格納
+  const inputText = document.getElementById("addText").value;
+  document.getElementById("addText").value = "";
+
+  //divタグ生成
+  const div = document.createElement("div");
+  console.log(div);
+};
+document
+  .getElementById("addButton")
+  .addEventListener("click", () => onClickAdd());
